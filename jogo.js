2f894397bcee74b8929a5f8942ed95d780152e8c
Programação PHP 3612952.2024.4 <36129522024.4@senacrjedu.local>
2024-09-23 18:29:23 -0300

    novo commit

---
+++ jogo.js
@@ -0,0 +1,22 @@
+number1 = 'pedra'
+number2 = 'papel'
+
+
+
+if(number1=='pedra' && number2=='tesoura'){
+    console.log('ganhou o jogador 1')
+}
+else if (number1=='tesora' && number2=='papel'){
+    console.log('ganhou o jogador 1')
+}
+else if (number1=='papel' && number2=='pedra'){
+    console.log('ganhou o  jogador1')
+}
+else if (number1===number2){
+    console.log('os dois jogadores perdem')
+}
+   
+else{
+    console.log('o jogador 2 ganha')
+}
+
