b393e82c5d2d476532bbafb32a7295e504265d5d
Programação PHP 3612952.2024.4 <36129522024.4@senacrjedu.local>
2024-10-01 20:07:33 -0300

    aula 12

--- aula11/conta.php
+++ aula11/conta.php
@@ -7,11 +7,13 @@ class Conta{
     public $saldo;
     public $numeroDaConta;
     public $titular;
+    public $extrato;
 
     public function __construct($titular, $numeroDaConta, $saldo=0) {
         $this->titular = $titular;
         $this->numeroDaConta = $numeroDaConta;
         $this->saldo = $saldo;
+        $this->extrato = [];
     }
     public function saque($valor){
 // valor que quero sacar
@@ -19,9 +21,10 @@ class Conta{
             echo "valor invalido \n";
         }
         else if($this->saldo>= $valor){
-            $this->saldo= $this->saldo - $valor;;
-            echo "saquue realizado com sucesso!.\n
+            $this->saldo= $this->saldo - $valor;
+            $saida = "saque realizado com sucesso!.\n 
             Saldo Atual: $this->saldo.\n";
+            array_push($this->extrato, $saida);
         }
         else{
             echo "Saldo insuficiente \n";
@@ -35,14 +38,53 @@ class Conta{
         }
         elseif($valor>0){
             $this->saldo += $valor;
-            echo" deposito realizado com sucesso.\n";
-            echo " saldo atual:R$ $this->saldo.\n";
+            $saida = " deposito realizado com sucesso.\n";
+            $saida = $saida . " saldo atual:R$ $this->saldo.\n";
+            array_push($this->extrato, $saida);
         }
         else{
             echo"valor inválido";
         }
     }
+    public function transferencia($destinatario, $valor){
+        if ($valor <= 0){
+            echo "Valor inválido.\n";
+        }
+        elseif ($this->saldo >= $valor){
+            $this->saldo -= $valor;
+            $destinatario->saldo += $valor;
+            $saida = "transferencia efetuada com sucesso.\n";
+            $saida = $saida . "$destinatario->titular recebeu $valor.\n";
+            $saida = $saida . "Saldo atual: $this->saldo.\n";
+            array_push($this->extrato, $saida);
+
+            $saida2 = "transferencia efetuada com sucesso.\n";
+            $saida2 = $saida2 . "$this->titular enviou $valor.\n";
+            $saida2 = $saida2 . "Saldo atual: $destinatario->saldo.\n";
+            array_push($destinatario->extrato, $saida2);
+
+
+        }
+        else{
+            echo "Saldo insuficiente.\n";
+        }
+    }
+
+    public function verificarExtrato(){
+        echo "extrato do(a) $this->titular.\n";
+        foreach($this->extrato as $operacao){
+            echo $operacao;
+        }
+        echo "fim do extrato!";
+        echo str_repeat('-', 30)."\n";
+    }
     }
     $conta1 = new Conta("ana", "1520", 500);
-    $conta1->deposito(50);
+    $conta2 = new Conta("bia", "1528", 1500);
+    $conta1->saque(50);
+    $conta1->deposito(100);
+    $conta1->transferencia($conta2, 200);
+    $conta1->verificarExtrato();
+    $conta2->verificarExtrato();
+?>
 ?>
